1345522fcff0eabaac5b4853880348317adc59ca
MrManoj124 <manojmanorooban@gmail.com>
2026-02-08 08:37:25 +0530

    use multiple variables for type inference

--- Start.dart
+++ Start.dart
@@ -14,4 +14,7 @@ String name = 'Myne';
 bool isFlutterFun = true;
 
 //Type Inference (var)
-//var city = 'Binland'; // Dart infers this as a String
+var city = 'Binland'; // Dart infers this as a String
+var score = 95; // Dart infers this as an int
+var isRaining = false; // Dart infers this as a bool
+var temperature = 36.5; // Dart infers this as a double
