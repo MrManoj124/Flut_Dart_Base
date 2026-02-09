654661663c70d3569f36c436d919d01234bac104
MrManoj124 <manojmanorooban@gmail.com>
2026-02-09 22:56:34 +0530

    create passengername

---
+++ Start_Practice.dart
@@ -0,0 +1,12 @@
+//Exercise 01
+// passenger name
+String passengername="Meroon";
+// // driver name
+// String drivername="Kamal";
+
+// pickup location
+// drop location
+// fare
+// isDriverAssigned
+// Print a sentence using interpolation.
+
