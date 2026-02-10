9b3db8f57d04c5b510ab69243d0aed193e7a0dbd
MrManoj124 <manojmanorooban@gmail.com>
2026-02-10 20:10:08 +0530

    create function with return type

--- Start.dart
+++ Start.dart
@@ -247,3 +247,9 @@ void bookRide({
 });
 bookRide(pickup: "Colombo", drop: "Kandy"); // Output: Pickup: Colombo, Drop: Kandy
 bookRide(pickup: "Colombo", drop: "Kandy", distance: 115.5); // Output: Pickup: Colombo, Drop: Kandy, Distance: 115.5 km
+
+
+//Function with Return Type
+double calculateFare(double distance, double ratePerKm) {
+  return distance * ratePerKm;
+}
