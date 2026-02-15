6d6a5e6b3b51cd8515658d471c655d2c76e5613c
MrManoj124 <manojmanorooban@gmail.com>
2026-02-16 00:05:51 +0530

    print multiplication and division

--- Operator.dart
+++ Operator.dart
@@ -14,3 +14,5 @@ var a = 10;
 var b = 3;
 print("Addition: ${a + b}"); // Output: 13
 print("Subtraction: ${a - b}"); // Output: 7
+print("Multiplication: ${a * b}"); // Output: 30
+print("Division: ${a / b}"); // Output: 3.3333333333333335
