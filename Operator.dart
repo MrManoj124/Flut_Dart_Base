d60c38bdeb432cf88efd30c0bd2c0033bf632053
MrManoj124 <manojmanorooban@gmail.com>
2026-02-16 00:14:05 +0530

    print the values  check with or operator

--- Operator.dart
+++ Operator.dart
@@ -35,3 +35,4 @@ print("x <= y: ${x <= y}"); // Output: true
 var isSunny = true;
 var isWarm = false;
 print("Is it sunny and warm? ${isSunny && isWarm}"); // Output: false
+print("Is it sunny or warm? ${isSunny || isWarm}"); // Output: true
