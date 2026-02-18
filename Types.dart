ed6454f77c9489e862f0fe199ffb4422faca613a
MrManoj124 <manojmanorooban@gmail.com>
2026-02-18 23:27:58 +0530

    Define String and print the value

--- Types.dart
+++ Types.dart
@@ -3,3 +3,9 @@ var age  = 30; //int
 var price = 19.99; //double
 print("Age : ${age}"); 
 print("Price : ${price}");
+
+
+//Strings
+var name = "Manorooban";
+var greeting  = "Hello ,${name}!";
+print(greeting);  //Output : Hello , Manorooban!
