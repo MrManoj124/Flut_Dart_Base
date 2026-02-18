33b5e8305ff58864321f53bf94cdeb250d2589d1
MrManoj124 <manojmanorooban@gmail.com>
2026-02-18 23:30:44 +0530

    Define Boolean Variable and print the values

--- Types.dart
+++ Types.dart
@@ -9,3 +9,10 @@ print("Price : ${price}");
 var name = "Manorooban";
 var greeting  = "Hello ,${name}!";
 print(greeting);  //Output : Hello , Manorooban!
+
+
+//Booleans
+var isActive = true;
+var isLoggedIn = false ;
+print("Is Active : ${isActive}");  //Output  : Is Active  : true
+print("Is Logged In : ${isLoggedIn}"); //Output : Is Logged In : false
