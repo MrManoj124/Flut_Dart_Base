bfc386949592ab68cfd881d695a08f2851b39cc2
MrManoj124 <manojmanorooban@gmail.com>
2026-02-22 22:51:53 +0530

    Print AND, OR, AND NOT, XOR operators

--- Types.dart
+++ Types.dart
@@ -105,3 +105,8 @@ void main() {
 //Bitwise operators
 final value = 0x22;
 final bitmask = 0x0f;
+
+assert((value & bitmask) == 0x02); // AND
+assert((value & ~bitmask) == 0x20); // AND NOT
+assert((value | bitmask) == 0x2f); // OR
+assert((value ^ bitmask) == 0x2d); // XOR
