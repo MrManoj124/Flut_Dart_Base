c40fbcef583048d21aaa6d71deb7973e06222301
MrManoj124 <manojmanorooban@gmail.com>
2026-02-23 23:41:51 +0530

    Define gifts as a constructor

--- Types.dart
+++ Types.dart
@@ -199,3 +199,11 @@ var gifts = {
 };
 
 var nobleGases = {2: 'helium', 10: 'neon', 18: 'argon'};
+
+//Define Map Constructor
+var gifts = Map<String, String>();
+gifts['first'] = 'partridge';
+gifts['second'] = 'turtledoves';
+gifts['fifth'] = 'golden rings';
+
+
