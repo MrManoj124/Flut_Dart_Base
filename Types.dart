40dc30f11d22563b021ad9dc0ac669e4aa40d696
MrManoj124 <manojmanorooban@gmail.com>
2026-02-23 23:42:15 +0530

    Deine nobleGases as second constructor

--- Types.dart
+++ Types.dart
@@ -206,4 +206,8 @@ gifts['first'] = 'partridge';
 gifts['second'] = 'turtledoves';
 gifts['fifth'] = 'golden rings';
 
+var nobleGases = Map<int, String>();
+nobleGases[2] = 'helium';
+nobleGases[10] = 'neon';
+nobleGases[18] = 'argon';
 
