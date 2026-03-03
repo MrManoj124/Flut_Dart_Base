6730066ab81d1fb98d2cce18a1de9e895719e5ac
MrManoj124 <manojmanorooban@gmail.com>
2026-03-03 16:19:10 +0530

    Define a example code for record

--- Records.dart
+++ Records.dart
@@ -1,3 +1,9 @@
 //Record Syntax
 var record = ('first', a:2, b:true, 'last');
 
+//Record type Annotations are comma-delimited lists 
+//of types enclosed in parentheses.
+(int, int) swap((int, int) record) {
+    var(a,b) = record;
+    return(b,a);
+}
