e368144b680f1fa78d054ba71fae92537cdab279
MrManoj124 <manojmanorooban@gmail.com>
2026-03-03 16:13:58 +0530

    Runes and grapheme clusters

--- Types.dart
+++ Types.dart
@@ -284,4 +284,12 @@ var itemsF = {?absentKey: ?absentValue}; // {}
 
 
 
+//Runes and grapheme clusters
+import 'package:characters/characters.dart';
 
+void main() {
+  var hi = 'Hi 🇩🇰';
+  print(hi);
+  print('The end of the string: ${hi.substring(hi.length - 1)}');
+  print('The last character: ${hi.characters.last}');
+}
