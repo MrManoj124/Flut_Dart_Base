9715d8bdcbe2c09a4ed636b4006c52814eb55b1e
MrManoj124 <manojmanorooban@gmail.com>
2026-03-21 21:43:26 +0530

    create a set that's a compile-time constant

--- Collections.dart
+++ Collections.dart
@@ -33,7 +33,18 @@ var elements =<String>{};
 
 
 //Use .length to get the number of items in the set:
-var elements = <String>{};
+var elements2 = <String>{};
 //elements.add('fluorine');
 //elements.addAll(halogens);
 //assert(elements.length == 5);
+
+
+//To create a set that's a compile-time constant, add const before the set literal
+final constantSet = const {
+  'fluorine',
+  'chlorine',
+  'bromine',
+  'iodine',
+  'astatine',
+};
+// constantSet.add('helium'); // This line will cause an error.
