77db9de6256913eaa98f6325d8f39f9756f86139
MrManoj124 <manojmanorooban@gmail.com>
2026-03-21 21:59:47 +0530

    Add a new key-value pair to an existing map

--- Collections.dart
+++ Collections.dart
@@ -75,3 +75,8 @@ var nobleGase = Map<int, String>();
 nobleGase[2] = 'helium';
 nobleGase[10] = 'neon';
 nobleGase[18] = 'argon';
+
+
+//Add a new key-value pair to an existing map using the subscript assignment operator ([]=): 
+var gifts = {'first' : 'Partidge'};
+gifts['fourth']='calling birds'; // Add a key-value pair
