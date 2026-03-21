00e005a72f5f422beb16560250e189a238b8fe86
MrManoj124 <manojmanorooban@gmail.com>
2026-03-21 21:30:43 +0530

    Create a simple compil-time constant

--- Collections.dart
+++ Collections.dart
@@ -6,3 +6,7 @@ var list2 = ['Car', 'Bus', 'Lorry', 'Bike'];
 var listed = [10,20,30];
 //assert(listed.length == 30);
 //assert(listed[1] == 20);
+
+//Create a simple compil-time constant
+var constantList = const[5,15,25];
+//constantlist[1] = 1; //This line will couse error
