fac784103c2191f36353ad795d682ba2125b2a01
MrManoj124 <manojmanorooban@gmail.com>
2026-03-27 00:51:35 +0530

    Create uniqueNames variable

--- Generics.dart
+++ Generics.dart
@@ -31,3 +31,4 @@ abstract class Cache<T> {
 //Using collection literals
 //List, set and map literals can be parameterized
 var names = <String>['Servar', 'Kathy', 'Ellam','server'];
+var uniqueNames = <String>['Servar', 'Kathy', 'Ellam','server'];
