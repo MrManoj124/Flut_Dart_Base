2ebe376a8928aeda5f65e659a2b26bf45987cc0a
MrManoj124 <manojmanorooban@gmail.com>
2026-03-26 23:01:58 +0530

    Exmaple01 for Generics

---
+++ Generics.dart
@@ -0,0 +1,8 @@
+//Generics
+//If you intend for a list to contain only strings, 
+//you can declare it as List<String> (read that as "list of string").
+
+//Example for Generics
+var names = <String>[];
+names.addAll(['servar', 'Ellam','kaiyider','server']);
+names.add(51);
