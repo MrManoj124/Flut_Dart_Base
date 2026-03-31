6cbec9836ee45505897b398d73f88d8d6779db54
MrManoj124 <manojmanorooban@gmail.com>
2026-03-31 23:39:51 +0530

    inline Function types

--- Typedefs.dart
+++ Typedefs.dart
@@ -4,3 +4,7 @@ IntList il = [1,2,3,4];
 //A type alias can have type parameters
 typedef ListMapper<X> = map<X, List<X>>;
 Map<String, List<String>> m1 = {}; //Verbose
+
+
+//inline Function types
+typedef Compare<T> = int Function(T a, T b);
