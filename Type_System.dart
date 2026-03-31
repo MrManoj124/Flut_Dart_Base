68dbae69ca4f64599f979d351f3313b66a70646b
MrManoj124 <manojmanorooban@gmail.com>
2026-04-01 00:19:40 +0530

    create main function

--- Type_System.dart
+++ Type_System.dart
@@ -1,2 +1,9 @@
 void printInts(List<int> a) => print(a);
 void printStrings(List<String> a) => print(a);
+
+void main(){
+  final list = [];
+  list.add(1);
+  list.add('2');
+  printInts(list); //Runtime error
+}
