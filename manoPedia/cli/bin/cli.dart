a141b1d74b386aaa912536947beef292300cf923
MrManoj124 <manojmanorooban@gmail.com>
2026-04-20 22:09:11 +0530

    Add another statement and print it

--- manoPedia/cli/bin/cli.dart
+++ manoPedia/cli/bin/cli.dart
@@ -2,4 +2,5 @@ import 'package:cli/cli.dart' as cli;
 
 void main(List<String> arguments) {
   print('Hello Mr.Manoj: ${cli.calculate()}!');
+  print('Hello Guys Welcome to Dart...!: ${cli.calculate()}!');
 }
