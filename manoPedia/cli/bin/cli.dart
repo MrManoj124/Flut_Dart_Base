9d2a6669703ff98f8d6f994f66851feb56b7b511
MrManoj124 <manojmanorooban@gmail.com>
2026-04-20 22:19:42 +0530

    Add a version in cli.dart file

--- manoPedia/cli/bin/cli.dart
+++ manoPedia/cli/bin/cli.dart
@@ -1,5 +1,7 @@
 import 'package:cli/cli.dart' as cli;
 
+const version = '0.0.1';
+
 void main(List<String> arguments) {
   print('Hello Mr.Manoj: ${cli.calculate()}!');
   print('Hello Guys Welcome to Dart...!: ${cli.calculate()}!');
