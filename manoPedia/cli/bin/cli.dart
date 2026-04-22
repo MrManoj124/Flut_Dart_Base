a3c099138a3318e5ca8e99d759b974590ba56967
MrManoj124 <manojmanorooban@gmail.com>
2026-04-22 19:23:32 +0530

    Add a function for Call the searchWikipedia function from the main function

--- manoPedia/cli/bin/cli.dart
+++ manoPedia/cli/bin/cli.dart
@@ -2,7 +2,7 @@ import 'package:cli/cli.dart' as cli;
 
 const version = '0.0.1';
 
-void main(List<String> arguments) {
+ /* void main(List<String> arguments) {
   //print('Hello Mr.Manoj: ${cli.calculate()}!');
   //print('Hello Guys Welcome to Dart...!: ${cli.calculate()}!');
   /* if(arguments.isEmpty){
@@ -29,4 +29,8 @@ void searchWickipedia(List<String>? arguments){
 // Highlights from the preceding code:
 // List<String>? arguments means that the arguments list itself can be null.
 
-}
+} */
+ 
+
+//Call the searchWikipedia function from the main function
+//  modify the search command block in main to call searchWikipedia
