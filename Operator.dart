eb982c233abc28c421162bb20c12bda84ced583e
MrManoj124 <manojmanorooban@gmail.com>
2026-04-23 07:40:31 +0530

    Add a operation for all the basic logical operations

--- Operator.dart
+++ Operator.dart
@@ -225,3 +225,8 @@ assert(a != b); // -1 != 0
 final value = 0x22;
 final bitmask = 0x0f;
 
+assert((value & bitmask) == 0x02); // AND
+assert((value & ~bitmask) == 0x20); // AND NOT
+assert((value | bitmask) == 0x2f); // OR
+assert((value ^ bitmask) == 0x2d); // XOR
+
