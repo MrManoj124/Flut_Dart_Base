78c6b34fe1b6910ddda2f392c263bfd2c5d7233e
MrManoj124 <manojmanorooban@gmail.com>
2026-05-28 23:09:07 +0530

    Add a call backs array with for loop

--- Loops.dart
+++ Loops.dart
@@ -10,3 +10,12 @@ var numbers = [1, 2, 3, 4, 5];
 for (var number in numbers) {
   print(number); // Output: 1 2 3 4 5
 }
+
+// Closures inside of Dart's for loops capture the value of the index.
+var callbacks = [];
+for (var i = 0; i < 3; i++) {
+  callbacks.add(() => print(i));
+}
+
+for (final c in callbacks) {
+  c(); // Output: 0 1 2
