af492cacc51b06a7c43eb15b48d056e9e34673a6
MrManoj124 <manojmanorooban@gmail.com>
2026-05-28 23:07:00 +0530

    Add an example for loop with a list

--- Loops.dart
+++ Loops.dart
@@ -3,4 +3,10 @@ var message = StringBuffer('Dart is fun');
 for (var i = 0; i < 5; i++) {
   message.write('!');
 }
-print(message); // Output: Dart is fun!!!!!
+print(message); // Output: Dart is fun
+
+// Example for loop with a list
+var numbers = [1, 2, 3, 4, 5];
+for (var number in numbers) {
+  print(number); // Output: 1 2 3 4 5
+}
